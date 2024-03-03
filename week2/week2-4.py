67238cba94abf5a3d4120961bee68d3a8a77acca
Yujung Gil <74306759+fairyroad@users.noreply.github.com>
2024-03-03 16:28:56 +0900

    Update week2-4.py

--- week2/week2-4.py
+++ week2/week2-4.py
@@ -1,42 +1,44 @@
+import copy
+
+
 def turn_left(i):
     if i == 0:
         return 3
     else:
         return i - 1
-N, M = map(int, input().split())
-x, y, i = map(int, input().split())
+
+N, M = map(int, input().split()) # 4, 4
+x, y, i = map(int, input().split()) # 1, 1, 0(N)
+
 graph =[]
 for _ in range(N):
     graph.append(list(map(int, input().split())))
-cnt = 0
+
+visited = copy.deepcopy(graph) # visited = [[0] * m for _ in range(n)]
 stop = 0
-dir = ((0,-1),(-1,0),(0,1),(1,0))
-print("================================================")
-print(x, y)
+dir = ((-1, 0),(0,1),(1,0),(0,-1))
+visited[x][y] = 1
+cnt = 1
+
 while True:
     nx = x + dir[i][0]
     ny = y + dir[i][1]
-    print(nx, ny)
-    if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
-        i = turn_left(i)
-        print("direction changed:", i)
+    i = turn_left(i)
+    if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == 0 and graph[nx][ny] == 0:
         x = nx
         y = ny
-        graph[x][y] = 0
+        visited[x][y] = 1
         cnt += 1
         stop = 0
-        print("current location:", x, y)
-        print("current count:", cnt)
     else:
-        i = turn_left(i)
         stop += 1
-        print("oops there's no available move. direction changed", i)
     if stop == 4:
-        y = ny + 1
-        x = nx
-        stop = 0
-        print("too many stops")
-        if ny >= M or graph[x][y] == 0:
-            print("ALL STOP")
+        ny = y - dir[i][1]
+        nx = x - dir[i][0]
+        if ny >= M or graph[x][y] == 1:
             break
-print(cnt)        
+        else:
+            y = ny
+            x = nx
+            stop = 0
+print(cnt)  
